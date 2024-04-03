61c7e1664494b422d196c49a3c45cac6656df2a2
Vadym <vrupa1713@gmail.com>
2024-04-03 22:14:51 +0200

    - feat(spider): added reviews spider; - feat(middleware): added middleware for proxy usage; - feat(settings): configured some settings;

--- scraper/settings.py
+++ scraper/settings.py
@@ -1,93 +1,79 @@
-# Scrapy settings for scraper project
-#
-# For simplicity, this file contains only settings considered important or
-# commonly used. You can find more settings consulting the documentation:
-#
-#     https://docs.scrapy.org/en/latest/topics/settings.html
-#     https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
-#     https://docs.scrapy.org/en/latest/topics/spider-middleware.html
+import os
+from distutils.util import strtobool
+
+from dotenv import load_dotenv
+
+load_dotenv()
 
 BOT_NAME = "scraper"
 
 SPIDER_MODULES = ["scraper.spiders"]
 NEWSPIDER_MODULE = "scraper.spiders"
 
-
-# Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = "scraper (+http://www.yourdomain.com)"
+USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36"
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
-# Configure maximum concurrent requests performed by Scrapy (default: 16)
-#CONCURRENT_REQUESTS = 32
+CONCURRENT_REQUESTS = 16
 
-# Configure a delay for requests for the same website (default: 0)
-# See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
-# See also autothrottle settings and docs
 #DOWNLOAD_DELAY = 3
-# The download delay setting will honor only one of:
-#CONCURRENT_REQUESTS_PER_DOMAIN = 16
-#CONCURRENT_REQUESTS_PER_IP = 16
 
 # Disable cookies (enabled by default)
-#COOKIES_ENABLED = False
+COOKIES_ENABLED = False
 
 # Disable Telnet Console (enabled by default)
-#TELNETCONSOLE_ENABLED = False
+TELNETCONSOLE_ENABLED = False
 
 # Override the default request headers:
-#DEFAULT_REQUEST_HEADERS = {
-#    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
-#    "Accept-Language": "en",
-#}
+DEFAULT_REQUEST_HEADERS = {
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+    "Accept-Language": "en-US,en",
+    "Accept-Encoding": "gzip, deflate, br",
+    "Connection": "keep-alive",
+    "User-Agent": USER_AGENT,
+}
 
-# Enable or disable spider middlewares
-# See https://docs.scrapy.org/en/latest/topics/spider-middleware.html
-#SPIDER_MIDDLEWARES = {
-#    "scraper.middlewares.ScraperSpiderMiddleware": 543,
-#}
+RETRY_TIMES = 3
+RETRY_HTTP_CODES = [302, 500, 502, 503, 504]
 
-# Enable or disable downloader middlewares
-# See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
-#DOWNLOADER_MIDDLEWARES = {
-#    "scraper.middlewares.ScraperDownloaderMiddleware": 543,
-#}
+DOWNLOAD_HANDLERS = {
+    'http': 'scraper.utils.handlers.RotatingProxiesDownloadHandler',
+    'https': 'scraper.utils.handlers.RotatingProxiesDownloadHandler'
+}
 
-# Enable or disable extensions
-# See https://docs.scrapy.org/en/latest/topics/extensions.html
-#EXTENSIONS = {
-#    "scrapy.extensions.telnet.TelnetConsole": None,
-#}
+DOWNLOADER_MIDDLEWARES = {
+    "scraper.middlewares.HttpProxyMiddleware": 500,
+    # "scrapy.downloadermiddlewares.retry.RetryMiddleware": 501
+    "scraper.middlewares.CustomRetryMiddleware": 501,
+}
 
-# Configure item pipelines
-# See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    "scraper.pipelines.ScraperPipeline": 300,
-#}
+EXTENSIONS = {
+    "scrapy.extensions.telnet.TelnetConsole": None,
+}
 
-# Enable and configure the AutoThrottle extension (disabled by default)
-# See https://docs.scrapy.org/en/latest/topics/autothrottle.html
-#AUTOTHROTTLE_ENABLED = True
-# The initial download delay
-#AUTOTHROTTLE_START_DELAY = 5
-# The maximum download delay to be set in case of high latencies
-#AUTOTHROTTLE_MAX_DELAY = 60
-# The average number of requests Scrapy should be sending in parallel to
-# each remote server
-#AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
-# Enable showing throttling stats for every response received:
-#AUTOTHROTTLE_DEBUG = False
-
-# Enable and configure HTTP caching (disabled by default)
-# See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
-#HTTPCACHE_ENABLED = True
-#HTTPCACHE_EXPIRATION_SECS = 0
-#HTTPCACHE_DIR = "httpcache"
-#HTTPCACHE_IGNORE_HTTP_CODES = []
-#HTTPCACHE_STORAGE = "scrapy.extensions.httpcache.FilesystemCacheStorage"
+ITEM_PIPELINES = {
+    "scraper.pipelines.ReviewsStoringPipeline": 300,
+}
 
 # Set settings whose default value is deprecated to a future-proof value
 REQUEST_FINGERPRINTER_IMPLEMENTATION = "2.7"
 TWISTED_REACTOR = "twisted.internet.asyncioreactor.AsyncioSelectorReactor"
 FEED_EXPORT_ENCODING = "utf-8"
+
+LOG_LEVEL = os.getenv("LOG_LEVEL", "INFO")
+LOG_FILE = os.getenv("LOG_FILE") if os.getenv("LOG_FILE", "") else None
+
+ROTATING_PROXIES_DOWNLOADER_HANDLER_AUTO_CLOSE_CACHED_CONNECTIONS_ENABLED = strtobool(
+    os.getenv("AUTO_CLOSE_CACHED_CONNECTIONS_ENABLED", "False")
+)
+
+PROXY = os.getenv("PROXY", "")
+PROXY_AUTH = os.getenv("PROXY_AUTH", "")
+PROXY_ENABLED = strtobool(os.getenv("PROXY_ENABLED", "False"))
+
+DB_HOST = os.getenv("DB_HOST", "127.0.0.1")
+DB_PORT = int(os.getenv("DB_PORT", "3306"))
+DB_USERNAME = os.getenv("DB_USERNAME", "root")
+DB_PASSWORD = os.getenv("DB_PASSWORD", "")
+DB_DATABASE = os.getenv("DB_DATABASE", "db_name")
---
+++ scraper/spiders/amazon_reviews.py
@@ -0,0 +1,76 @@
+from typing import Iterable
+from datetime import datetime
+import logging
+
+import scrapy
+from scrapy import Request
+
+from scraper.items import ReviewItem
+
+logger = logging.getLogger(__name__)
+
+
+class AmazonReviewsSpider(scrapy.Spider):
+    name = "amazon_reviews"
+    allowed_domains = ["www.amazon.com"]
+    product_code = "B01IPHVFUI"
+    reviews_url_format_str = "https://www.amazon.com/product-reviews/{code}?filterByStar={rating}&pageNumber={page_number}&language=en_US&sortBy=relevancy&formatType=current_format"
+    rating_numbers_to_string_filter = {1: "one_star", 2: "two_star", 3: "three_star", 4: "four_star", 5: "five_star"}
+
+    def start_requests(self) -> Iterable[Request]:
+        for rating_number in range(1, 6):
+            rating_str = self.rating_numbers_to_string_filter[rating_number]
+            yield self.get_next_review_request(rating_str, 1)
+        return
+
+    def get_next_review_request(self, rating, next_page_number) -> Request:
+        review_url = self.reviews_url_format_str.format(code=self.product_code,
+                                                        rating=rating,
+                                                        page_number=next_page_number)
+        return Request(url=review_url,
+                       callback=self.parse_reviews,
+                       dont_filter=True,
+                       meta={'request_url': review_url},
+                       cb_kwargs={"page_number": next_page_number, "rating": rating})
+
+    def parse_reviews(self, response, page_number, rating):
+        for review_element in response.xpath("//div[@data-hook='review']"):
+            review_body = self._get_review_body(review_element)
+            rating_number = self._get_review_rating(review_element)
+            author = self._get_review_author(review_element)
+            post_date = self._get_review_post_date(review_element)
+
+            yield ReviewItem(review_body=review_body, rating_number=rating_number, author=author, post_date=post_date)
+
+        if self._is_last_review_pagination_page(response):
+            logger.info(f"Last reviews page. product: {self.product_code} | page: {page_number} | rating: {rating}")
+            return
+
+        next_page_number = page_number + 1
+        yield self.get_next_review_request(rating, next_page_number)
+
+    @staticmethod
+    def _get_review_body(review_element):
+        return review_element.xpath(".//*[contains(@data-hook, 'review-body')]/span").get()
+
+    @staticmethod
+    def _get_review_rating(review_element):
+        rating_str = review_element.xpath(
+            ".//i[@data-hook='review-star-rating' and contains(@class, 'a-star-')]/@class").re_first(r'a-star-(\d+)',
+                                                                                                     default='0')
+        return float(rating_str)
+
+    @staticmethod
+    def _get_review_author(review_element):
+        return review_element.xpath(".//*[@class='a-profile-name']/text()").get()
+
+    @staticmethod
+    def _get_review_post_date(review_element):
+        raw_post_date_str: str = review_element.xpath(".//*[@data-hook='review-date']/text()").get()
+        raw_post_date_str = raw_post_date_str.replace(",", "").replace(".", "")
+        date_str = " ".join(raw_post_date_str.split(" ")[-3:])
+        return datetime.strptime(date_str, "%B %d %Y").date()
+
+    @staticmethod
+    def _is_last_review_pagination_page(response) -> bool:
+        return not response.xpath("//li[contains(@class, 'a-last') and not(contains(@class, 'a-disabled'))]")
