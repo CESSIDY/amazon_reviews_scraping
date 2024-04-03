c1df2801b0a7208e433bd217308255c9ca725240
Vadym <vrupa1713@gmail.com>
2024-04-03 23:33:32 +0200

    - feat(models): added Review model and alembic version; - feat(pipeline): added logic for reviews saving; - fix(scraper): fixed rating scraping;

--- scraper/items.py
+++ scraper/items.py
@@ -2,6 +2,9 @@ import scrapy
 
 
 class ReviewItem(scrapy.Item):
+    product_code = scrapy.Field()
+    external_id = scrapy.Field()
+
     review_body = scrapy.Field()
     rating_number = scrapy.Field()
     author = scrapy.Field()
--- scraper/pipelines.py
+++ scraper/pipelines.py
@@ -1,6 +1,33 @@
 from itemadapter import ItemAdapter
 
+from sqlalchemy import Engine, create_engine, cast, Integer
+from sqlalchemy.orm import Session
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.dialects.mysql import insert
+from utils.mysql_connection_string import mysql_connection_string
+
+from database.models import Review
+from items import ReviewItem
+
 
 class ReviewsStoringPipeline:
-    def process_item(self, item, spider):
+    def __init__(self):
+        self.engine: Engine = create_engine(mysql_connection_string())
+        self.session = sessionmaker(self.engine)
+
+    def process_item(self, item: ReviewItem, spider):
+        with self.session.begin() as session:
+            review = {
+                "product_code": item.get("product_code"),
+                "external_id": item.get("external_id"),
+                "review_body": item.get("review_body"),
+                "rating_number": item.get("rating_number"),
+                "author": item.get("author"),
+                "post_date": item.get("post_date"),
+            }
+
+            stmt = insert(Review)
+            stmt = stmt.on_duplicate_key_update(review).values(review)
+
+            session.execute(stmt)
         return item
